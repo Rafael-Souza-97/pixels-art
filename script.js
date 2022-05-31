0024169755e8e712b2e456e08d6035c909ffa69e
Rafael Souza <souza_rafael.97@outlook.com>
2022-05-31 12:05:22 -0300

    Resolve problemas do Lint

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
-const tabelaPixels = document.querySelector('#pixel-board');
 const color = document.querySelectorAll('.color');
 const botao = document.getElementById('clear-board');
 const vqv = document.getElementById('generate-board');
 const inputBtn = document.getElementById('board-size');
-const pixel = document.querySelector('pixel');
+
+function selectedColor() {
+  const colors = document.querySelectorAll('.color');
+  for (let index = 0; index < colors.length; index += 1) {
+    colors[index].addEventListener('click', (event) => {
+      const searchSelected = document.querySelector('.selected');
+      searchSelected.classList.remove('selected');
+      event.target.classList.add('selected');
+    });
+  }
+}
+selectedColor();
+
+function applyColor() {
+  const pixels = document.querySelectorAll('.pixel');
+  for (let index = 0; index < pixels.length; index += 1) {
+    pixels[index].addEventListener('mousedown', (event) => {
+      const searchSelected = document.querySelector('.selected');
+      const select = getComputedStyle(searchSelected).backgroundColor;
+      const eventTarget = event.target;
+      eventTarget.style.backgroundColor = select;
+    });
+  }
+}
+applyColor();
+
+function resetButton() {
+  const pixel = document.querySelectorAll('.pixel');
+  botao.addEventListener('click', () => {
+    for (let index = 0; index < pixel.length; index += 1) {
+      pixel[index].style.backgroundColor = 'white';
+    }
+  });
+}
+resetButton();
 
 function geraDivs(size) {
   const sizeCollum = size;
@@ -39,40 +72,6 @@ vqv.addEventListener('click', () => {
   }
 });
 
-function selectedColor() {
-  const colors = document.querySelectorAll('.color');
-  for (let index = 0; index < colors.length; index += 1) {
-    colors[index].addEventListener('click', (event) => {
-      let searchSelected = document.querySelector('.selected');
-      searchSelected.classList.remove('selected');
-      event.target.classList.add('selected');
-    });
-  }
-}
-selectedColor();
-
-function applyColor() {
-  const pixels = document.querySelectorAll('.pixel');
-  for (let index = 0; index < pixels.length; index += 1) {
-    pixels[index].addEventListener('mousedown', (event) => {
-      let searchSelected = document.querySelector('.selected');
-      let select = getComputedStyle(searchSelected).backgroundColor;
-      event.target.style.backgroundColor = select;
-    });
-  }
-}
-applyColor();
-
-function resetButton() {
-  let pixel = document.querySelectorAll('.pixel');
-  botao.addEventListener('click', () => {
-    for (let index = 0; index < pixel.length; index += 1) {
-      pixel[index].style.backgroundColor = 'white';
-    }
-  });
-}
-resetButton();
-
 // Mentoria Débora:
 
 function randomColors() {
@@ -80,7 +79,7 @@ function randomColors() {
     const red = parseInt(Math.random() * 255, 10);
     const green = parseInt(Math.random() * 255, 10);
     const blue = parseInt(Math.random() * 255, 10);
-    let rgb = `rgb(${red}, ${green}, ${blue})`;
+    const rgb = `rgb(${red}, ${green}, ${blue})`;
     color[index].style.backgroundColor = rgb;
   }
 }
